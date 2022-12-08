80d2595be8f4a074660f95c6ccb0e97d413c85cb
Fabian Schober <fabian@dirtypaws.studio>
2022-12-08 14:31:48 +0100

    Simplified Template

--- template.ts
+++ template.ts
@@ -1,35 +1,20 @@
 import { Linq } from "https://deno.land/x/linqts@1.0.7/mod.ts";
 import { lodash as _ } from "https://deno.land/x/deno_ts_lodash@0.0.1/mod.ts";
 
-enum InputSource {
-  TestData = "Test Data",
-  TestFile = "Test File",
-  ProdFile = "Prod File",
-}
+const L = <T>(array: T[]): Linq<T> => new Linq<T>(array);
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Config ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const INPUT_SOURCE = InputSource.TestFile as InputSource;
+const PROD = false;
+const inputFile = PROD ? "input.txt" : "input-test.txt";
+console.log("Using " + inputFile);
 
-const TEST_DATA = ``;
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-console.log("Using " + INPUT_SOURCE);
-const input = INPUT_SOURCE === InputSource.TestData
-  ? TEST_DATA
-  : await Deno.readTextFile(
-    INPUT_SOURCE === InputSource.TestFile ? "input-test.txt" : "input.txt",
-  );
-
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const input = await Deno.readTextFile(inputFile);
 const inputLines = input.trim().split(/\r?\n/g).map((l) => l.trim());
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Here be Dragons ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 console.time("time");
 
+// CODE
 
 console.timeEnd("time");
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-function L<T>(array: T[]): Linq<T> {
-  return new Linq<T>(array);
-}
