6f527322edc5cef34ae5b9586463d3c160a3bdfd
Fabian Schober <fabian@dirtypaws.studio>
2022-12-14 00:51:40 +0100

    Day 13

---
+++ 13/13-1.ts
@@ -0,0 +1,90 @@
+import { Linq } from "https://deno.land/x/linqts@1.0.7/mod.ts";
+import { lodash as _ } from "https://deno.land/x/deno_ts_lodash@0.0.1/mod.ts";
+
+const L = <T>(array: T[]): Linq<T> => new Linq<T>(array);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Config ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const PROD = true;
+const inputFile = PROD ? "input.txt" : "input-test.txt";
+console.log("Using " + inputFile);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const input = await Deno.readTextFile(inputFile);
+const inputLines = input.trim().split(/\r?\n/g).map((l) => l.trim());
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Here be Dragons ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+console.time("time");
+
+enum ValidationResult {
+    Valid = "Valid",
+    Invalid = "Invalid",
+    Continue = "Continue",
+}
+
+type Content = number | [] | undefined;
+
+const pairLines = chunkArrayByValue(inputLines, "");
+const pairs = pairLines.map(([str1, str2]) => [JSON.parse(str1), JSON.parse(str2)]);
+
+let correctIndices = new Array<number>();
+
+for (let i = 0; i < pairs.length; i++) {
+    const [packet1, packet2] = pairs[i];
+    let toCompare: [Content, Content][] = _.zip(packet1, packet2); // Nice typing
+
+    let validation = ValidationResult.Continue;
+    while (toCompare.length > 0 && validation === ValidationResult.Continue) {
+        const [left, right] = toCompare.shift()!;
+        console.log(`${i + 1}: ${JSON.stringify(left)} and ${JSON.stringify(right)}`)
+
+        if (left == null && right == null) {
+            throw new Error("This should never happen");
+        } else if (left == null) {
+            validation = ValidationResult.Valid;
+        } else if (right == null) {
+            validation = ValidationResult.Invalid;
+        } else if (Number.isFinite(left) && Number.isFinite(right)) {
+            validation = left === right
+                ? ValidationResult.Continue
+                : (left < right ? ValidationResult.Valid : ValidationResult.Invalid);
+        } else if (left instanceof Array && right instanceof Array) {
+            const newEntries: [Content, Content][] = _.zip(left, right);
+            toCompare = newEntries.concat(toCompare);
+        } else if (left instanceof Array && Number.isFinite(right)) {
+            const newEntries: [Content, Content][] = _.zip(left, [right]);
+            toCompare = newEntries.concat(toCompare);
+        } else if (Number.isFinite(left) && right instanceof Array) {
+            const newEntries: [Content, Content][] = _.zip([left], right);
+            toCompare = newEntries.concat(toCompare);
+        } else {
+            throw new Error("What the heck should I validate?");
+        }
+    }
+
+    console.log(`\t ->  ${validation}`);
+    if (validation === ValidationResult.Valid) {
+        correctIndices.push(i + 1);
+    }
+}
+
+console.log("Correct indices:")
+console.dir(correctIndices);
+console.log(correctIndices.reduce((a, b) => a + b, 0));
+
+console.timeEnd("time");
+
+// Functions
+function chunkArrayByValue<T>(array: T[], value: T): T[][] {
+    const chunks: T[][] = [];
+    let chunk: T[] = [];
+    for (const item of array) {
+        if (item === value) {
+            chunks.push(chunk);
+            chunk = [];
+        } else {
+            chunk.push(item);
+        }
+    }
+    chunks.push(chunk);
+    return chunks;
+}
